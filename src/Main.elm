02e5803e5072f34a55a17e9b087132bd80e512fb
drryner <drryner@icloud.com>
2019-01-15 15:21:10 +0500

    Update example

--- src/Main.elm
+++ src/Main.elm
@@ -9,20 +9,23 @@ import Utils exposing (..)
 import Counter
 import Widget exposing (Widget)
 
-type Msg = CounterMsg Counter.Msg Int | AddCounter
+type Msg = CounterMsg Counter.Msg Int | AddCounter | CounterSingleMsg Counter.Msg
 
 type alias Model =
     { counters : Widget.Box Counter.Msg Counter.Model Msg
+    , counter : Widget Counter.Msg Counter.Model Msg
     }
 
 init : () -> (Model, Cmd Msg)
 init _ = pure
     { counters = Widget.newBox CounterMsg Counter.info
+    , counter = Widget.new CounterSingleMsg Counter.info
     }
 
 view : Model -> Html Msg
 view model = div []
     [ label [] [ text "Widget test" ]
+    , Widget.show model.counter
     , button [ onClick AddCounter ] [ text "Add counter" ]
     , Widget.showBox model.counters
     ]
@@ -31,10 +34,14 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = case msg of
     CounterMsg msg_ id -> let (m, c) = Widget.updateBox_ msg_ id model.counters in ({ model | counters = m }, c)
     AddCounter         -> pure { model | counters = Widget.add_ model.counters }
+    CounterSingleMsg msg_ -> let (m, c) = Widget.update_ msg_ model.counter in ({ model | counter = m}, c)
 
 
 subs : Model -> Sub Msg
-subs model = Widget.subsBox model.counters
+subs model = Sub.batch
+    [ Widget.subsBox model.counters
+    , Widget.subs model.counter
+    ]
 
 main = Browser.element { init = init, view = toUnstyled << view, update = update, subscriptions = subs }
 
